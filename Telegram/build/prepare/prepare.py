690660eb50d8b1599720705f873986bdb56ecc79
Vladimir Malkov <vladimir.malkov@varian.com>
2026-07-30 13:58:51 +0300

    Use GitHub mirror for dav1d

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -743,7 +743,7 @@ win:
 
 # Somehow in x86 Debug build dav1d crashes on AV1 10bpc videos.
 stage('dav1d', """
-    git clone -b 1.5.3 https://code.videolan.org/videolan/dav1d.git
+    git clone -b 1.5.3 https://github.com/videolan/dav1d.git
     cd dav1d
 win32:
     SET "TARGET=x86"
